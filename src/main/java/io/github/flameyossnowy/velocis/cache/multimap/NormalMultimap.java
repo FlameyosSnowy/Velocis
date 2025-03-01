0c3cc21e819f591d065c0d691cfb47ad3f172196
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-03-01 12:54:32 +0200

    New multimaps

---
+++ src/main/java/io/github/flameyossnowy/velocis/cache/multimap/NormalMultimap.java
@@ -0,0 +1,7 @@
+package io.github.flameyossnowy.velocis.cache.multimap;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class NormalMultimap<K, V> extends HashMap<K, List<V>> implements Multimap<K, V> {
+}
