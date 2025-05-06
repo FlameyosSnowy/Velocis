6d865cc48a0fed0210d9b075f8197d73e9771ae2
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-05-06 17:37:52 +0300

    module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module io.github.flameyossnowy.velocis {
+    exports io.github.flameyossnowy.velocis.cache;
+    exports io.github.flameyossnowy.velocis.cache.algorithms;
+    exports io.github.flameyossnowy.velocis.cache.multimap;
+    exports io.github.flameyossnowy.velocis.tables;
+}
