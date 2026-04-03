cbb2419b989aff9c5a3e1af4be1d6ad84424e476
flameyosflow <flamesfflowsnow@gmail.com>
2026-04-03 18:59:29 +0200

    Add new methods

--- src/main/java/io/github/flameyossnowy/velocis/cache/algorithms/ConcurrentDoublyLinkedList.java
+++ src/main/java/io/github/flameyossnowy/velocis/cache/algorithms/ConcurrentDoublyLinkedList.java
@@ -1,18 +1,27 @@
 package io.github.flameyossnowy.velocis.cache.algorithms;
 
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.concurrent.atomic.AtomicReference;
 
-class ConcurrentDoublyLinkedList<K, V> {
+@ApiStatus.Internal
+public class ConcurrentDoublyLinkedList<K, V> {
     private final AtomicReference<Node<K, V>> head;
     private final AtomicReference<Node<K, V>> tail;
 
     public ConcurrentDoublyLinkedList() {
-        head = new AtomicReference<>(null);
-        tail = new AtomicReference<>(null);
+        head = new AtomicReference<>();
+        tail = new AtomicReference<>();
     }
 
     public Node<K, V> addToEnd(K key, V value) {
         Node<K, V> node = new Node<>(key, value);
+        insertAtEnd(node);
+        return node;
+    }
+
+    public Node<K, V> addToEnd(Node<K, V> node) {
         insertAtEnd(node);
         return node;
     }
@@ -35,7 +44,7 @@ class ConcurrentDoublyLinkedList<K, V> {
         tail.set(head.get());
     }
 
-    private void insertAtEnd(Node<K, V> node) {
+    private void insertAtEnd(@NotNull Node<K, V> node) {
         Node<K, V> oldTail;
         do {
             oldTail = tail.get();
@@ -53,5 +62,13 @@ class ConcurrentDoublyLinkedList<K, V> {
         if (node == null) return;
         node.prev.next = node.next;
         node.next.prev = node.prev;
+    }
+
+    public AtomicReference<Node<K, V>> getHead() {
+        return head;
+    }
+
+    public AtomicReference<Node<K, V>> getTail() {
+        return tail;
     }
 }
